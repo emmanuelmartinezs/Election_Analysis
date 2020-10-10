0d30b30e340c8b51da94d4a4d043a4a6930aa482
Emmanuel <71406941+emmanuelmartinezs@users.noreply.github.com>
2020-10-10 02:53:17 -0400

    Update PyPoll_Module

--- PyPoll_Module.py
+++ PyPoll_Module.py
@@ -6,10 +6,20 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Open the election results and read the file.
+# 1. Initialize a total vote counter.
+total_votes = 0
+
+# Open the election results and read the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    # Print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+
+# 3. Print the total votes.
+print(f'{total_votes:,.0f}')
