f45b176f144c0bdbb0b36a605d210d9f578e8fa8
Emmanuel <71406941+emmanuelmartinezs@users.noreply.github.com>
2020-10-10 03:16:55 -0400

    Update PyPull_Module

--- PyPoll_Module.py
+++ PyPoll_Module.py
@@ -6,20 +6,40 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# 1. Initialize a total vote counter.
+# Initialize a total vote counter.
 total_votes = 0
 
-# Open the election results and read the file
+# Candidate Options
+candidate_options = []
+# Declare the empty dictionary.
+candidate_votes = {}
+
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
     # Read the header row.
     headers = next(file_reader)
 
-    # Print each row in the CSV file.
+# Print each row in the CSV file.
     for row in file_reader:
-        # 2. Add to the total vote count.
+        # Add to the total vote count.
         total_votes += 1
 
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+            # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+# Print the candidate list.
+print(candidate_votes)
+
 # 3. Print the total votes.
-print(f'{total_votes:,.0f}')
+#print(f'{total_votes:,.0f}')
