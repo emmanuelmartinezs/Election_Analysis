e4c0e39099c1b092cd6957327c09ae79b560a587
Emmanuel <71406941+emmanuelmartinezs@users.noreply.github.com>
2020-10-10 02:32:04 -0400

    Update PyPoll_Module

--- PyPoll_Module.py
+++ PyPoll_Module.py
@@ -8,8 +8,6 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-
-    # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
     # Print the header row.
