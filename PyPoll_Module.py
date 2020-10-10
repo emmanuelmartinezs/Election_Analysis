25295bd6cb2a11a04e5580ec5407850658955e19
Emmanuel <71406941+emmanuelmartinezs@users.noreply.github.com>
2020-10-10 03:26:54 -0400

    Update PyPull_Module

--- PyPoll_Module.py
+++ PyPoll_Module.py
@@ -38,8 +38,12 @@ with open(file_to_load) as election_data:
         # Add a vote to that candidate's count.
         candidate_votes[candidate_name] += 1
 
-# Print the candidate list.
-print(candidate_votes)
-
-# 3. Print the total votes.
-#print(f'{total_votes:,.0f}')
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
